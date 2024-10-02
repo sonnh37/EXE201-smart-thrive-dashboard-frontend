25b6613f3ef082060c4e1b4433d09ea3b81c1695
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-10-02 20:56:38 +0700

    update sidebar

--- smart-thrive-dashboard/src/components/common/admin-panel-layout.tsx
+++ smart-thrive-dashboard/src/components/common/admin-panel-layout.tsx
@@ -16,48 +16,38 @@ export default function AdminPanelLayout({
   const sidebar = useStore(useSidebarToggle, (state) => state);
 
   if (!sidebar) return null;
-  
+
   return (
-    <>
-      <motion.div
-        className="grid"
-        initial="hidden"
-        animate="show"
-        viewport={{ once: true }}
-        variants={{
-          hidden: {},
-          show: {
-            transition: {
-              staggerChildren: 0.15,
-            },
+    <motion.div
+      initial="hidden"
+      animate="show"
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
           },
-        }}
+        },
+      }}
+    >
+      <Sidebar />
+      <main
+        className={cn(
+          "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
+          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
+        )}
       >
-        <motion.div
-          className="fixed z-50"
-          variants={Const.FADE_RIGHT_ANIMATION_VARIANTS}
-        >
-          <Sidebar />
-        </motion.div>
-        <div>
-          <main
-            className={cn(
-              "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
-              sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
-            )}
-          >
-            {children}
-          </main>
-          <footer
-            className={cn(
-              "transition-[margin-left] ease-in-out duration-300",
-              sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
-            )}
-          >
-            <Footer />
-          </footer>
-        </div>
-      </motion.div>
-    </>
+        {children}
+      </main>
+      <footer
+        className={cn(
+          "transition-[margin-left] ease-in-out duration-300",
+          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
+        )}
+      >
+        <Footer />
+      </footer>
+    </motion.div>
   );
 }
--- smart-thrive-dashboard/src/components/layouts/sidebar.tsx
+++ smart-thrive-dashboard/src/components/layouts/sidebar.tsx
@@ -8,18 +8,28 @@ import { Menu } from "@/components/common/menu";
 import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
 import { SidebarToggle } from "@/components/common/sidebar-toggle";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { Const } from "@/lib/const";
+import { useEffect } from "react";
 
 export function Sidebar() {
   const sidebar = useStore(useSidebarToggle, (state) => state);
+  const isMobile = window.innerWidth < 768; // Thay đổi 768 nếu cần
 
+  useEffect(() => {
+    if (isMobile && sidebar?.isOpen) {
+      sidebar.setIsOpen(); // Đóng sidebar khi là mobile
+    }
+  }, [isMobile, sidebar?.isOpen]);
   if (!sidebar) return null;
 
   return (
-    <aside
+    <motion.aside
       className={cn(
-        "top-0 left-0 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
-        sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+        "fixed top-0 left-0 z-20 h-screen transition-[width] ease-in-out duration-300",
+        sidebar.isOpen ? "w-72" : "w-[90px] -translate-x-full"
       )}
+      variants={Const.FADE_RIGHT_ANIMATION_VARIANTS}
     >
       <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
       <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800">
@@ -38,8 +48,7 @@ export function Sidebar() {
               height={50}
               alt="Gallery Icon"
               className="rounded-full aspect-square object-cover bg-gradient-to-r from-indigo-500 to-sky-500 [box-shadow:1px_1px_20px_var(--tw-shadow-color)] shad shadow-blue-500"
-              />
-            {/* <PanelsTopLeft className="w-6 h-6 mr-1"/> */}
+            />
             <h1
               className={cn(
                 "[text-shadow:1px_1px_20px_var(--tw-shadow-color)] shadow-blue-500 font-bold text-lg whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300",
@@ -54,6 +63,6 @@ export function Sidebar() {
         </Button>
         <Menu isOpen={sidebar?.isOpen} />
       </div>
-    </aside>
+    </motion.aside>
   );
 }
