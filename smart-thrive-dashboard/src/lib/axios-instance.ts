e946efc397be20baadbf070f7d13c69fbbc5ebc0
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-10-22 23:04:39 +0700

    update middleware

--- smart-thrive-dashboard/src/lib/axios-instance.ts
+++ smart-thrive-dashboard/src/lib/axios-instance.ts
@@ -1,42 +1,42 @@
 import axios from "axios";
-import {logout} from "./auth";
+import { logout } from "./auth";
 
 // Tạo một instance của axios (nếu cần)
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE, // URL cơ bản cho các request API của bạn
-    timeout: 10000, // Thời gian chờ tối đa cho request (ms)
+  baseURL: process.env.NEXT_PUBLIC_API_BASE, // URL cơ bản cho các request API của bạn
+  timeout: 10000, // Thời gian chờ tối đa cho request (ms)
 });
 
 // Thêm Interceptor để tự động đính kèm Authorization token cho tất cả request
 axiosInstance.interceptors.request.use(
-    async (config) => {
-        // Lấy token từ localStorage hoặc cookie
-        const token = localStorage.getItem("token");
-        config.headers['ngrok-skip-browser-warning'] = 'true';
+  (config) => {
+    // Lấy token từ localStorage hoặc cookie
+    const token = localStorage.getItem("token");
+    // config.headers['ngrok-skip-browser-warning'] = 'true';
 
-        // Kiểm tra tính hợp lệ của token
-        if (token) {
-            // Đính kèm token vào header Authorization nếu có token
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+    // Kiểm tra tính hợp lệ của token
+    if (token) {
+      // Đính kèm token vào header Authorization nếu có token
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 // Interceptor để xử lý phản hồi lỗi
 axiosInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        // Kiểm tra nếu token đã hết hạn
-        if (error.response?.status === 401) {
-            logout();
-        }
-        return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    // Kiểm tra nếu token đã hết hạn
+    if (error.response?.status === 401) {
+      logout();
     }
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
